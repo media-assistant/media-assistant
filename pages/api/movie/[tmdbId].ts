c29f388215321a4ceeef8e80280e19859caad6f8
Niels Bik <nielsrowinbik@gmail.com>
2022-02-28 20:55:48 +0100

    Begin work on API to add and remove movies

--- pages/api/movie/[tmdbId].ts
+++ pages/api/movie/[tmdbId].ts
@@ -1,15 +1,20 @@
 import type { ApiError, Movie } from "@/lib/types";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getMovie } from "@/lib/movies";
+import { addMovie, deleteMovie, getMovie } from "@/lib/movies";
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<Movie | ApiError>
 ) => {
-  if (req.method === "GET") {
-    const tmdbId = parseInt(req.query.tmdbId as string);
-    const title = (req.query.title as string).toLowerCase();
+  const tmdbId = parseInt(req.query.tmdbId as string);
+  const title = (req.query.title as string).toLowerCase();
 
+  if (req.method === "DELETE") {
+    await deleteMovie(tmdbId);
+    return res.status(200);
+  }
+
+  if (req.method === "GET") {
     const movie = await getMovie(tmdbId, title);
 
     if (movie) return res.status(200).json(movie);
@@ -17,8 +22,8 @@ const handler = async (
   }
 
   if (req.method === "POST") {
-    // TODO: Figure out how to add a movie so that Radarr will
-    // have the download client download it.
+    const movie = await addMovie(tmdbId, title);
+    return res.status(201).json(movie);
   }
 
   return res.status(501);
