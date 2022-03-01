e1b29aca21d99cd5989c6c4c56589be097542188
Niels Bik <nielsrowinbik@gmail.com>
2022-03-01 22:07:45 +0100

    Add docs to Movie API, prepare to list downloads

--- lib/movies.ts
+++ lib/movies.ts
@@ -1,11 +1,22 @@
 import type { Movie, RadarrMovie } from "./types";
 import { del, get, post } from "@/lib/fetch";
+import { getMovieQueue } from "./downloads";
 import qs from "qs";
 import slugify from "slugify";
 
 const apiKey = process.env.RADARR_API_KEY;
 const apiUrl = `${process.env.RADARR_URL}/api/v3`;
 
+/**
+ * Adds the specfied movie to the user's Radarr library.
+ *
+ * @param tmdbId he The Movie DB ID for the movie to add
+ * @param title The title of this movie
+ * @param [qualityProfileId] The quality profile to use when downloading this movie
+ * @param [rootFolderPath] The folder in which to place the files for this movie
+ * @returns The added movie
+ *
+ */
 export const addMovie = async (
   tmdbId: number,
   title: string,
@@ -28,6 +39,12 @@ export const addMovie = async (
   return transform(movie);
 };
 
+/**
+ * Removes the specfied movie to the user's Radarr library.
+ *
+ * @param tmdbId The The Movie DB ID for the movie to add
+ *
+ */
 export const deleteMovie = async (tmdbId: number) => {
   // First, get the movie because we don't know its Radarr id:
   const movie = await getMovie(tmdbId);
@@ -41,55 +58,100 @@ export const deleteMovie = async (tmdbId: number) => {
   await del(`${apiUrl}/movie/${movie.id}?${params}`);
 };
 
-export const getMovie = async (
-  tmdbId: number,
-  title?: string
-): Promise<Movie | undefined> => {
+/**
+ * Finds and returns a movie within user's Radarr library.
+ *
+ * @param tmdbId The The Movie DB ID for the movie to find
+ * @returns The movie, if found, otherwise undefined
+ *
+ * @internal
+ */
+const _getMovieFromLibrary = async (tmdbId: number) => {
   const params = qs.stringify({ apiKey, tmdbId });
   const movieList = await get<RadarrMovie[]>(`${apiUrl}/movie?${params}`);
 
   // If the requested movie is part of the user's library, it should be
   // the only item returned by Radarr. If that is the case, return it:
   if (movieList.length === 1 && movieList[0].tmdbId === tmdbId) {
-    return transform(movieList[0]);
+    const movie = movieList[0];
+    // TODO: Get downloads for this movie
+    return transform(movie);
   }
 
-  // If the movie is not a part of the user's library, we can look it up
-  // by its title, but only if the title was provided. If it wasn't, there's
-  // nothing we can do:
-  if (!title) {
-    // Should we throw here, just like a failed `get` will throw?
-    return undefined;
-  }
+  // If not, this movie isn't in our library, return undefined:
+  return undefined;
+};
 
-  // Attempt to look up the movie:
+/**
+ * Finds and returns a specific movie through Radarr's lookup API.
+ *
+ * @param tmdbId The The Movie DB ID for the movie to find
+ * @returns The movie, if found, otherwise undefined
+ *
+ * @internal
+ */
+const _getMovieFromLookup = async (tmdbId: number, title: string) => {
   const lookupList = await searchMovies(title);
 
   // The movie we're looking for should be among the
   // search results, so look for its TMDB ID:
-  const movie = lookupList.find((result) => result.tmdbId === tmdbId);
-
-  // If the movie is not within the search results, there's nothing we can do:
-  if (!movie) {
-    // Should we throw here, just like a failed `get` will throw?
-    return undefined;
-  }
-
-  // If it was, cool! Return it:
-  return movie;
+  return lookupList.find((result) => result.tmdbId === tmdbId);
 };
 
+/**
+ * Finds and returns a specific movie. Looks within the user's Radarr library first,
+ * and falls back to Radarr's lookup API if the title is provided.
+ *
+ * @param tmdbId The The Movie DB ID for the movie to find
+ * @param [title] The title of the movie to find
+ * @returns The movie, if found, otherwise undefined
+ *
+ */
+export const getMovie = async (
+  tmdbId: number,
+  title?: string
+): Promise<Movie | undefined> =>
+  (await _getMovieFromLibrary(tmdbId)) ??
+  (title && (await _getMovieFromLookup(tmdbId, title)));
+
+/**
+ * Finds and returns all movies in the user's Radarr library.
+ *
+ * @returns An array of movies
+ *
+ */
 export const getAllMovies = async (): Promise<Movie[]> => {
   const params = qs.stringify({ apiKey });
   const movieList = await get<RadarrMovie[]>(`${apiUrl}/movie?${params}`);
-  return movieList.map(transform);
+
+  return movieList.map((movie) => {
+    // TODO: For each movie, check if we have an active download
+    const downloads = [];
+    return transform(movie, downloads);
+  });
 };
 
+/**
+ * Finds and returns a subset of movies in the user's Radarr library,
+ * sorted by when they were added to the library.
+ *
+ * @param [limit] The amount of movies to return
+ * @returns An array of movies
+ *
+ */
 export const getRecentMovies = async (limit: number = 10): Promise<Movie[]> => {
   const movies = await getAllMovies();
   return movies.sort((a, b) => +b.added - +a.added).slice(0, limit);
 };
 
+/**
+ * Wrapper around Radarr's lookup API.
+ *
+ * @param term The search query to use
+ * @param [limit] The amount of movies to return
+ * @returns An array of movies
+ *
+ */
 export const searchMovies = async (
   term: string,
   limit: number = 4
@@ -103,29 +165,38 @@ export const searchMovies = async (
     `${apiUrl}/movie/lookup?${params}`
   );
 
-  return movieList.slice(0, limit).map(transform);
+  return movieList.slice(0, limit).map((movie) => transform(movie));
 };
 
 // Function to convert Radarr's API response to our own:
-const transform = ({
-  added,
-  certification,
-  genres,
-  hasFile,
-  title,
-  monitored,
-  overview,
-  images,
-  ratings,
-  runtime,
-  year,
-  youTubeTrailerId,
-  id,
-  tmdbId,
-}: RadarrMovie): Movie => ({
+const transform = (
+  {
+    added,
+    certification,
+    genres,
+    hasFile,
+    title,
+    monitored,
+    overview,
+    images,
+    ratings,
+    runtime,
+    year,
+    youTubeTrailerId,
+    id,
+    tmdbId,
+  }: RadarrMovie,
+  downloads: unknown[] = []
+): Movie => ({
   added: added === "0001-01-01T00:00:00Z" ? false : new Date(added),
   canWatch: hasFile,
   certification,
+  // TODO: implement this transform
+  // TODO: move this transform elsewhere?
+  downloads: downloads.map(() => ({
+    progress: 0,
+    status: "downloading",
+  })),
   fanart: images[1]?.remoteUrl,
   genres,
   id,
