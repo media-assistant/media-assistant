2f7121a263024c46f798557782bba1dfb8050b9c
Niels Bik <nielsrowinbik@gmail.com>
2022-03-02 15:56:09 +0100

    Use SSR to get movies, remove some API routes, improve search

--- pages/api/movie/[tmdbId].ts
+++ pages/api/movie/[tmdbId].ts
@@ -1,24 +1,14 @@
-import type { ApiError, Movie } from "@/lib/types";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { addMovie, deleteMovie, getMovie } from "@/lib/movies";
+import { addMovie, deleteMovie } from "@/lib/movies";
+import type { Movie } from "@/lib/types";
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Movie | ApiError>
-) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<Movie>) => {
   const tmdbId = parseInt(req.query.tmdbId as string);
   const title = (req.query.title as string).toLowerCase();
 
   if (req.method === "DELETE") {
     await deleteMovie(tmdbId);
     return res.status(200);
-  }
-
-  if (req.method === "GET") {
-    const movie = await getMovie(tmdbId, title);
-
-    if (movie) return res.status(200).json(movie);
-    return res.status(404).json({ error: "Not Found" });
   }
 
   if (req.method === "POST") {
