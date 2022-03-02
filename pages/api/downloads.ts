901f58250b22b448123e818323c2a0eea77cb984
Niels Bik <nielsrowinbik@gmail.com>
2022-03-02 18:01:09 +0100

    Only allow GET method

--- pages/api/downloads.ts
+++ pages/api/downloads.ts
@@ -6,11 +6,15 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<Download[]>
 ) => {
-  const movieId = parseInt(req.query.movieId as string);
+  if (req.method === "GET") {
+    const movieId = parseInt(req.query.movieId as string);
 
-  const download = await getMovieQueue(movieId);
+    const download = await getMovieQueue(movieId);
 
-  return res.status(200).json(download);
+    return res.status(200).json(download);
+  }
+
+  return res.status(501).end();
 };
 
 export default handler;
